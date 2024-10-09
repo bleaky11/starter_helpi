ae6d08d5064827df503a9f9cf7cd547dc68b1c34
Thomas <tflorio@udel.edu>
2024-10-09 13:28:12 -0400

    Set up Basic Career Page component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
+import { basicCareerComponent } from './basicCareer';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -46,6 +47,7 @@ function App() {
         <br></br>
         <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
       </Form>
+      {basicCareerComponent}
     </div>
   );
 }
---
+++ src/basicCareer.tsx
@@ -0,0 +1,10 @@
+
+export function basicCareerComponent(): JSX.Element
+{
+    <div>
+        Here is the Basic Career Page!
+
+        <a href = "https://bleaky11.github.io/starter_helpi/"> Go to Home</a>
+    </div>
+}
+
