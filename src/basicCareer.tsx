666b90e81905cae0a11bab2b8fd31f79ae802379
DracoMeteorStorm <adracometeorstorm@gmail.com>
2024-10-11 13:26:24 -0400

    started questions

--- src/basicCareer.tsx
+++ src/basicCareer.tsx
@@ -4,6 +4,26 @@ export function BasicCareerComponent(): JSX.Element
     <div>
         Here is the Basic Career Page!
 
+
+        {/*  
+        Question 1.
+        How much noise do you mind in your work environment?
+        Question 2.
+        What type of environment would you prefer to work in?
+        Question 3.
+        Are you interested in any STEM fields?
+        Question 4.
+        Would you be fine doing manual labor?
+        Question 5.
+        How much teamwork 
+        Question 6.
+        Question 7.
+        Question 8.
+        Question 9.
+        Question 10.
+        
+        
+        */}
         <a href = "https://bleaky11.github.io/starter_helpi/"> Go to Home</a>
     </div>)
 }
