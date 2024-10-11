115a72ded87c2aec84173bbe797c344d1c75296f
Levi Chen <129811757+bleaky11@users.noreply.github.com>
2024-10-11 12:43:36 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
 import { HomePage } from './homepagelogo';
-//import { basicCareerComponent } from './basicCareer';
+import { BasicCareerComponent } from './basicCareer';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
