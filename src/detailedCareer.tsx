75d5f971100723625b4d68bc1a258c1275ccf248
DracoMeteorStorm <adracometeorstorm@gmail.com>
2024-10-18 13:24:07 -0400

    created states and dummy questions

--- src/detailedCareer.tsx
+++ src/detailedCareer.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import { backgroundStyle } from "./CSS/Background";
 import { headingStyle } from "./CSS/Heading";
 
 export function DetailedCareerComponent(): JSX.Element {
+
+  const [answerStatus, setAnswer] = useState<boolean[]>([false, false,false,false,false,false,false,false]);
+
+  function changeAnswer(questNum: number) {
+      
+  } 
+  
   return (
     <div style={backgroundStyle}>
       <div>
@@ -21,6 +29,21 @@ export function DetailedCareerComponent(): JSX.Element {
           Before you begin, make sure you're in a comfortable environment and
           answer each question to your best ability.
         </h5>
+        <div style={{textAlign:"center"}}>
+        <h3>Question 1.</h3>
+
+        <h3>Question 2.</h3>
+        <h3>Question 3.</h3>
+        <h3>Question 4.</h3>
+        <h3>Question 5.</h3>
+        <h3>Question 6.</h3>
+        <h3>Question 7.</h3>
+        <h3>Question 8.</h3>
+        <h3>Question 9.</h3>
+
+        <h3>Question 10.</h3>
+
+        </div>
       </div>
     </div>
   );
