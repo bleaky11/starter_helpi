ec9b967c5d902283a29c1f75f4702688668cf7b6
Ryan <rburtson@udel.edu>
2024-10-21 16:48:30 -0400

    Changed Link color to Black

--- src/homepagelogo.tsx
+++ src/homepagelogo.tsx
@@ -1,6 +1,6 @@
 export function HomePage (): JSX.Element{
     return<div>
-        <a href="https://bleaky11.github.io/starter_helpi/">
+        <a href="https://bleaky11.github.io/starter_helpi/" style={{color: "black"}}>
             <h1>The Career Quiz</h1>
         </a>
     </div>
