94d203c918b993a7f03010ccd045f35e4275161a
Thomas <tflorio@udel.edu>
2024-10-24 15:01:51 -0400

    Started branch

--- src/homepagelogo.tsx
+++ src/homepagelogo.tsx
@@ -1,5 +1,6 @@
 export function HomePage (): JSX.Element{
     return<div>
+        <div style = {{float: "left"}}>Login</div>
         <a href="https://bleaky11.github.io/starter_helpi/" style={{color: "black"}}>
             <h1>The Career Quiz</h1>
         </a>
