c9de21d5aafd29774adf7d6e1b1692f6fd8b9691
Thomas <tflorio@udel.edu>
2024-12-01 20:57:04 -0500

    fixed db issue

--- src/db.tsx
+++ src/db.tsx
@@ -19,7 +19,7 @@ export const initializeDatabase = async () => {
         const objectStore = db.createObjectStore("users", { keyPath: "username" });
         objectStore.createIndex("username", "username", { unique: true });
         objectStore.createIndex("loggedIn", "loggedIn", { unique: false });
-        objectStore.createIndex("basicComplete", "basicComplete", {unique: true});
+        objectStore.createIndex("basicComplete", "basicComplete", {unique: false});
       } 
     };
   });
