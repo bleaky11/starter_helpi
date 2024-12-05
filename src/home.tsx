9b48513ecc6724b908d551872c89c2bfd01f31f5
bleaky11 <levichen533@gmail.com>
2024-12-05 10:16:50 -0500

    fixed basic and detailed box leaving screen at extreme zoom

--- src/home.tsx
+++ src/home.tsx
@@ -30,7 +30,7 @@ export function MainPage({
         ></NotifBell>
         <Container>
           <Col className="ResultBorder" >
-              <div style={{ flex: 2 }}>
+              <div style={{ flex: 1 }}>
                 <h2>Results Page</h2>
                 <h6 className="Body-Heading" style={{fontSize: "16px" }}>
                   Results of the quiz will be displayed after completing basic and/or detailed question
@@ -55,7 +55,7 @@ export function MainPage({
             
         </Container>
         <Container>
-          <Row>
+          <Row style={{ display:'flex', flexWrap:'wrap', maxWidth:'100%' }}>
             <Col
               className="Bordered"
               style={{ display: "flex", flexDirection: "column" }}
@@ -131,7 +131,7 @@ export function MainPage({
               className="Bordered"
               style={{ display: "flex", flexDirection: "column" }}
             >
-              <div style={{ flex: 1 }}>
+              <div>
                 <h2>Detailed Career Assessment</h2>
                 <h6 className="Body-Heading" style={{fontSize: "16px" }}>
                   You will be asked a series of elaborate questions that may
